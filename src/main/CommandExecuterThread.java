c02e0adebec4af6eaa0a35c56ff517ccad649bc3
iChemy <controlyuu.dev@gmail.com>
2026-01-20 12:41:49 +0900

    :construction: WIP

--- src/main/CommandExecuterThread.java
+++ src/main/CommandExecuterThread.java
@@ -9,14 +9,39 @@ import org.json.JSONObject;
 
 import jp.vstone.RobotLib.CCommUMotion;
 import jp.vstone.RobotLib.CRobotMem;
+import jp.vstone.RobotLib.CRobotPose;
 
 public class CommandExecuterThread implements Runnable {
     private BlockingQueue<JSONObject> commandQueue;
     private Logger logger;
+    private final Byte[] HOME_POSITION_IDS = new Byte[]{
+        CCommUMotion.SV_BODY_P,
+        CCommUMotion.SV_BODY_Y,
+        CCommUMotion.SV_L_SHOULDER_P,
+        CCommUMotion.SV_L_SHOULDER_R,
+        CCommUMotion.SV_R_SHOULDER_P,
+        CCommUMotion.SV_R_SHOULDER_R,
+        CCommUMotion.SV_HEAD_P,
+        CCommUMotion.SV_HEAD_R,
+        CCommUMotion.SV_HEAD_Y,
+        CCommUMotion.SV_EYE_P,
+        CCommUMotion.SV_L_EYE_Y,
+        CCommUMotion.SV_R_EYE_Y,
+        CCommUMotion.SV_EYELIDs,
+        CCommUMotion.SV_MOUTH
+
+    };
+    private final Short[] HOME_POSITION_VALUES = new Short[]{
+        -2, 0, 58, 2, -58, -2, 0, 0, 0, 0, 0, 0, 0, 0
+    };
+    private final CRobotPose HOME_POSITION;
 
     public CommandExecuterThread(BlockingQueue<JSONObject> commandQueue, Logger logger) {
         this.commandQueue = commandQueue;
         this.logger = logger;
+        CRobotPose home_position = new CRobotPose();
+        home_position.SetPose(HOME_POSITION_IDS, HOME_POSITION_VALUES);
+        this.HOME_POSITION = home_position;
     }
 
     @Override
@@ -35,6 +60,7 @@ public class CommandExecuterThread implements Runnable {
         }
 
         motion.ServoOn();
+        motion.play(this.HOME_POSITION, 1000);
 
         while (true) {
             try {
